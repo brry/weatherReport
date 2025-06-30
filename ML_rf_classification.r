b75a170df846924d3c36a7addb92a2ea2ed90ebd
brry <berryboessenkool@hotmail.com>
2025-06-30 15:42:58 +0200

    ML RF updates

--- ML_rf_classification.r
+++ ML_rf_classification.r
@@ -31,13 +31,13 @@ feature_data <- data.frame(t(feature_data)) # transpose
 rnames <- berryFunctions::l2df(strsplit(rownames(feature_data),".", fixed=TRUE))
 feature_data <- cbind(rnames, feature_data) ; rm(rnames)
 names(feature_data)[1:2] <- c("id", "condition") 
-View(feature_data) # n observations should be >> n columns in real life
+View(feature_data) # n observations should be >> n columns in real life    !!!
 feature_data$condition <- as.factor(feature_data$condition)
 
 # random forest ----
 
 # Split data into training and testing sets:
-set.seed(641) # reproducibility
+set.seed(641) # reproducibility (see "ML_rf_experiments.r" !)
 train_indices <- sample(1:nrow(feature_data), nrow(feature_data)*0.7)
 train_data <- feature_data[train_indices, -1]
 test_data <- feature_data[-train_indices, -1]
@@ -54,5 +54,5 @@ caret::confusionMatrix(predictions, test_data$condition)#$overall['Accuracy']
 berryFunctions::sortDF(randomForest::importance(rf_model), 3, quiet=TRUE)
 randomForest::varImpPlot(rf_model, main="Random Forest Feature Importance", n.var=20)
 
-plot(x_q70~x_q60, data=feature_data)
-plot(m_q70~m_q60, data=feature_data)
+berryFunctions::linReg(m_q70~m_q60, data=feature_data)
+berryFunctions::linReg(x_q70~x_q60, data=feature_data) # our features are not good  !!!
--- ML_rf_experiments.r
+++ ML_rf_experiments.r
@@ -2,7 +2,6 @@
 # Berry Boessenkool, HPI course Data Science for Wearables, June 2025
 
 # accuracy of simple (known) relationship ----
-if(FALSE){ # for sourcing file
 
 feature_data <- data.frame(x=runif(30), y=runif(30), z=runif(30))
 feature_data$condition <- ifelse(feature_data$x>0.3 & feature_data$y>0.3, "b", "c")
@@ -18,11 +17,9 @@ predictions <- predict(rf_model, test_data)
 message(caret::confusionMatrix(predictions, test_data$condition)$overall['Accuracy'])
 randomForest::varImpPlot(rf_model, main="Random Forest Feature Importance")
 
-}
-
 
 # train/test proportion for actual data ----
-if(FALSE){ 
+
 feature_data <- data.frame(apply(imu_data, 3:4, get_stats))
 feature_data <- data.frame(t(feature_data)) # transpose
 feature_data <- cbind(berryFunctions::l2df(strsplit(rownames(feature_data),".", fixed=TRUE)), feature_data)
@@ -39,6 +36,7 @@ accuracy_prop <- function(prop){ # proportion used for training data
  predictions <- predict(rf_model, test_data)
  return(caret::confusionMatrix(predictions, test_data$condition)$overall['Accuracy'])
 }
+accuracy_prop(0.7)
 # spread of accuracy results based on random grouping train/test:
 pbapply::pbreplicate(80, accuracy_prop(0.7)) |> hist(breaks=20)
 replicate(50, accuracy_prop(0.7)) |> mean()
@@ -49,6 +47,4 @@ p <- seq(0.25, 0.95, by=0.01)
 set.seed(8) # avoid error: Need at least two classes to do classification.
 a <- pbapply::pbsapply(p, function(pr) replicate(50, accuracy_prop(pr)))
 berryFunctions::quantileBands(a, x=p)
-plot(p, colMeans(a))
-}
-
+plot(p, colMeans(a), las=1)
